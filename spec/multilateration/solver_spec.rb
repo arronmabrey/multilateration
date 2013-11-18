d2e7ac3af8235d173670fd8989e4616c5c503bbf
Arron Mabrey <arron@mabreys.com>
2013-11-18 00:49:52 -0500

    Rename TimeOfArrivalStrategy to TimeOfArrivalStrategies:: SourceVectorDistance

--- spec/multilateration/solver_spec.rb
+++ spec/multilateration/solver_spec.rb
@@ -1,26 +1,28 @@
 require 'spec_helper'
 
-class TimeOfArrivalStrategy
-  attr_reader :source_vector, :wave_speed
-  private :source_vector
+module TimeOfArrivalStrategies
+  class SourceVectorDistance
+    attr_reader :source_vector, :wave_speed
+    private :source_vector
 
-  def initialize(source_vector, wave_speed=1)
-    @source_vector = source_vector
-    @wave_speed    = wave_speed
-  end
+    def initialize(source_vector, wave_speed=1)
+      @source_vector = source_vector
+      @wave_speed    = wave_speed
+    end
 
-  def tdoa(vector_a, vector_b)
-    toa(vector_a) - toa(vector_b)
-  end
+    def tdoa(vector_a, vector_b)
+      toa(vector_a) - toa(vector_b)
+    end
 
-  def toa(target_vector)
-    distance_between_source_vector_and(target_vector) / wave_speed
-  end
+    def toa(target_vector)
+      distance_between_source_vector_and(target_vector) / wave_speed
+    end
 
-  private
+    private
 
-  def distance_between_source_vector_and(target_vector)
-    Math.sqrt((target_vector - source_vector).map { |component| component.abs**2 }.reduce(&:+))
+    def distance_between_source_vector_and(target_vector)
+      Math.sqrt((target_vector - source_vector).map { |component| component.abs**2 }.reduce(&:+))
+    end
   end
 end
 
@@ -32,7 +34,7 @@ describe Multilateration::Solver do
     let(:reciver_vector_2) { Vector[50,50,33]      }
     let(:reciver_vector_3) { Vector[23,75,4]       }
     let(:reciver_vector_4) { Vector[100,-100,-5.5] }
-    let(:time_of_arrival_strategy) { TimeOfArrivalStrategy.new(source_vector) }
+    let(:time_of_arrival_strategy) { TimeOfArrivalStrategies::SourceVectorDistance.new(source_vector) }
     let(:recivers) { [reciver_vector_0, reciver_vector_1, reciver_vector_2, reciver_vector_3, reciver_vector_4] }
 
     subject(:solved_vector) { described_class.new(recivers, time_of_arrival_strategy).solved_vector }
