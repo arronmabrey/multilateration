fc75c91b4e78a40006d1565d44658cead9233de5
Arron Mabrey <arron@mabreys.com>
2014-12-14 21:16:23 -0500

    remove old TimeOfArrivalStrategies::SourceVectorDistance

--- lib/multilateration.rb
+++ lib/multilateration.rb
@@ -6,7 +6,6 @@ require "multilateration/version"
 require "multilateration/signal_event_data_generator"
 require "multilateration/signal_event"
 require "multilateration/solver"
-require "multilateration/time_of_arrival_strategies/source_vector_distance"
 
 module Multilateration
   # Your code goes here...
